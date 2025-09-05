b78ef23d2f2e7b4c542c7dfde89fd61f1d5bf60d
Nick Müller <nick.mueller@mailbox.org>
2025-09-05 13:40:33 +0200

    feat(vacs-server): skip tracing of healthcheck endpoint and favicon

--- vacs-server/src/routes.rs
+++ vacs-server/src/routes.rs
@@ -3,13 +3,15 @@ mod root;
 mod ws;
 
 use crate::state::AppState;
+use axum::http::Request;
 use axum::Router;
 use axum_login::{AuthManagerLayer, AuthnBackend};
 use std::sync::Arc;
 use tower_http::timeout::TimeoutLayer;
-use tower_http::trace::{DefaultMakeSpan, TraceLayer};
-use tower_sessions::SessionStore;
+use tower_http::trace::{DefaultMakeSpan, MakeSpan, TraceLayer};
 use tower_sessions::service::SignedCookie;
+use tower_sessions::SessionStore;
+use tracing::Span;
 
 pub fn create_app<B, S>(auth_layer: AuthManagerLayer<B, S, SignedCookie>) -> Router<Arc<AppState>>
 where
@@ -20,7 +22,16 @@ where
         .nest("/auth", auth::routes())
         .nest("/ws", ws::routes().merge(crate::ws::routes()))
         .merge(root::routes())
-        .layer(TraceLayer::new_for_http().make_span_with(DefaultMakeSpan::default()))
+        .layer(
+            TraceLayer::new_for_http().make_span_with(move |req: &Request<_>| {
+                let path = req.uri().path();
+                match path {
+                    "/health" | "/favicon.ico" => Span::none(),
+                    _ => DefaultMakeSpan::default().make_span(req),
+                }
+            }),
+        )
+        .merge(root::untraced_routes())
         .layer(TimeoutLayer::new(crate::config::SERVER_SHUTDOWN_TIMEOUT))
         .layer(auth_layer)
 }
--- vacs-server/src/routes/root.rs
+++ vacs-server/src/routes/root.rs
@@ -1,22 +1,26 @@
 use crate::state::AppState;
-use axum::Router;
 use axum::routing::get;
+use axum::Router;
 use std::sync::Arc;
 
 pub fn routes() -> Router<Arc<AppState>> {
+    Router::new().route("/version", get(get::version))
+}
+
+pub fn untraced_routes() -> Router<Arc<AppState>> {
     Router::new()
         .route("/health", get(get::health))
-        .route("/version", get(get::version))
+        .route("/favicon.ico", get(get::favicon))
 }
 
 mod get {
     use crate::build::VersionInfo;
     use crate::http::ApiResult;
     use crate::state::AppState;
-    use axum::Json;
     use axum::extract::State;
     use axum::http::StatusCode;
     use axum::response::IntoResponse;
+    use axum::Json;
     use std::sync::Arc;
     use std::time::Duration;
 
@@ -25,6 +29,10 @@ mod get {
             Ok(Ok(_)) => (StatusCode::OK, "OK"),
             _ => (StatusCode::SERVICE_UNAVAILABLE, "Service Unavailable"),
         }
+    }
+
+    pub async fn favicon() -> impl IntoResponse {
+        StatusCode::NOT_FOUND
     }
 
     pub async fn version() -> ApiResult<VersionInfo> {
